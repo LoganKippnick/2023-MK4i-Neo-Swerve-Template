75483a3527328b525d85f5d722e6bdf8513d1b48
Logan-Kippnick <lgkippnick@outlook.com>
2023-01-25 19:53:46 -0500

    example command and subsystem

---
+++ src/main/java/frc/robot/subsystems/ExampleSys.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ExampleSys extends SubsystemBase {
+
+    // Declare actuators, sensors, and other variables here
+
+    /**
+     * Constructs a new ExampleSys.
+     * 
+     * <p>ExampleCmd contains the basic framework of a robot subsystem for use in command-based programming.
+     */
+    public ExampleSys() {
+        // Initialize and configure actuators and sensors here
+    
+    }
+
+    // This method will be called once per scheduler run
+    @Override
+    public void periodic() {
+        
+    }
+
+    // Put methods for controlling this subsystem here. Call these from Commands.
+
+}
