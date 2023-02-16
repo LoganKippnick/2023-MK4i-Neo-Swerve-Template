1534d13394d0106cca79608349b3620e06cbcd4b
FRC6081 <firstrtm@gmail.com>
2023-02-15 18:41:58 -0600

    claw: basic commands

--- src/main/java/frc/robot/subsystems/ClawSys.java
+++ src/main/java/frc/robot/subsystems/ClawSys.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PneumaticChannels;
 
@@ -20,4 +21,11 @@ public class ClawSys extends SubsystemBase {
         
     }
     
+    public void open() {
+        clawSols.set(Value.kForward);
+    }
+
+    public void close() {
+        clawSols.set(Value.kReverse);
+    }
 }
