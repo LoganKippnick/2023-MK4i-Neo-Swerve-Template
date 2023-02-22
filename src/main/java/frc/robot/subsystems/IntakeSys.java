8eaf5c60b47b264774f2fa254b84ebde17237a41
FRC6081 <firstrtm@gmail.com>
2023-02-21 21:47:29 -0600

    intake: actuation

--- src/main/java/frc/robot/subsystems/IntakeSys.java
+++ src/main/java/frc/robot/subsystems/IntakeSys.java
@@ -1,11 +1,13 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANDevices;
 import frc.robot.Constants.IntakeConstants;
@@ -18,14 +20,16 @@ public class IntakeSys extends SubsystemBase {
     private final CANSparkMax upperMtr;
     private final CANSparkMax lowerMtr;
 
+    private final RelativeEncoder intakeEnc;
+
     private final SparkMaxPIDController controller;
 
     private double targetInches = 0;
 
     /**
-     * Constructs a new ExampleSys.
+     * Constructs a new IntakeSys.
      * 
-     * <p>ExampleSys contains the basic framework of a robot subsystem for use in command-based programming.
+     * <p>IntakeSys contains methods for control and actuation of the intake.
      */
     public IntakeSys() {
         // Initialize and configure actuators and sensors here
@@ -33,14 +37,20 @@ public class IntakeSys extends SubsystemBase {
         upperMtr = new CANSparkMax(CANDevices.upperMtrId, MotorType.kBrushless);
         lowerMtr = new CANSparkMax(CANDevices.lowerMtrId, MotorType.kBrushless);
 
-        controller = actuationMtr.getPIDController();
+        intakeEnc = actuationMtr.getEncoder();
+        intakeEnc.setPositionConversionFactor(IntakeConstants.encRevToInches);
 
+        controller = actuationMtr.getPIDController();
+        controller.setP(IntakeConstants.kP);
+        controller.setD(IntakeConstants.kD);
     }
 
     // This method will be called once per scheduler run
     @Override
     public void periodic() {
-        controller.setReference(targetInches, ControlType.kPosition);
+        // controller.setReference(targetInches, ControlType.kPosition);
+
+        SmartDashboard.putNumber("intake inches", intakeEnc.getPosition());
     }
 
     // Put methods for controlling this subsystem here. Call these from Commands.
