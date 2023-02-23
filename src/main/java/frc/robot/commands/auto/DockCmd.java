7ffb69f94123ffe1a00a6a5fff03102b28ec09ad
FRC6081 <firstrtm@gmail.com>
2023-02-23 13:38:24 -0600

    organization

--- src/main/java/frc/robot/commands/auto/DockCmd.java
+++ src/main/java/frc/robot/commands/auto/DockCmd.java
@@ -2,8 +2,6 @@ package frc.robot.commands.auto;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DockDirection;
