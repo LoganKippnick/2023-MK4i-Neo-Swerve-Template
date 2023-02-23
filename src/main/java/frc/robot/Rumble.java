8f5d68e1f92062a471f47119030644eadeb72697
Logan-Kippnick <lgkippnick@outlook.com>
2023-02-23 09:08:40 -0500

    rumble: pulse control

--- src/main/java/frc/robot/Rumble.java
+++ src/main/java/frc/robot/Rumble.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BooleanSupplier;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -13,7 +14,7 @@ public class Rumble {
     private final Joystick controller;
     private final RumbleType rumbleType;
     private final double power;
-    private final Timer buzzTimer;
+    private final Timer pulseTimer;
 
     private boolean isRumbling;
     /**
@@ -22,6 +23,15 @@ public class Rumble {
      */
     public boolean isRumbling() {
         return isRumbling;
+    }
+
+    private boolean isPulsing;
+    /**
+     * Returns whether the controller is pulsing.
+     * @return True if the controller is pulsing.
+     */
+    public boolean isPulsing() {
+        return isPulsing;
     }
 
     private boolean isEnabled;
@@ -33,7 +43,7 @@ public class Rumble {
         return isEnabled;
     }
     /**
-     * Sets whether the Rumble is enabled. If false, stops the rumbling and buzzing instantly.
+     * Sets whether the Rumble is enabled. If false, stops the rumbling and pulsing instantly.
      * @param isEnabled True if the Rumble should be enabled, false if it should be disabled.
      */
     public void setEnabled(boolean isEnabled) {
@@ -42,20 +52,28 @@ public class Rumble {
         if(!isEnabled) stop();
     }
 
-    private double buzzTime;
+    private double pulseTime;
     /**
-     * Returns the length of a buzz.
-     * @return The length of a buzz, in seconds.
+     * Returns the length of a pulse.
+     * @return The length of a pulse, in seconds.
      */
-    public double getBuzzTime() {
-        return buzzTime;
+    public double getPulseTime() {
+        return pulseTime;
     }
     /**
-     * Sets the length of a buzz.
-     * @param buzzTime The desired length of a buzz, in seconds.
+     * Sets the length of a pulse.
+     * @param pulseTime The desired length of a pulse, in seconds.
      */
-    public void setBuzzTime(double buzzTime) {
-        this.buzzTime = buzzTime;
+    public void setPulseTime(double pulseTime) {
+        this.pulseTime = pulseTime;
+    }
+
+    private double pulseGapTime = 0.33; // TODO: Javadoc
+    public double getPulseGapTime() {
+        return pulseGapTime;
+    }
+    public void setPulseGapTime(double pulseGapTime) {
+        this.pulseGapTime = pulseGapTime;
     }
     
     /**
@@ -69,18 +87,20 @@ public class Rumble {
         this.controller = controller;
         this.rumbleType = rumbleType;
         this.power = power;
-
-        buzzTimer = new Timer();
+        
+        pulseTimer = new Timer();
 
         isRumbling = false;
+        isPulsing = false;
         isEnabled = true;
-        buzzTime = 0.25;
+        pulseTime = 0.25;
     }
 
     /**
      * Starts the Rumble, if enabled.
      */
     public void start() {
+        isRumbling = true;
         if(isEnabled) controller.setRumble(rumbleType, power);
     }
 
@@ -88,18 +108,63 @@ public class Rumble {
      * Stops the Rumble.
      */
     public void stop() {
+        isRumbling = false;
         controller.setRumble(rumbleType, 0.0);
     }
 
     /**
-     * Controller rumbles for 0.25 seconds, unless set for a different length.
-     * <p>To change the length, use setBuzzTime.
+     * Controller rumbles for a short period of time once.
+     * <p>To change the length of a pulse, use setPulseTime.
      */
-    public void buzz() {
-        buzzTimer.start();
+    public void pulse() {
+        isPulsing = true;
+        pulseTimer.start();
         start();
-        Trigger stopper = new Trigger(() -> buzzTimer.hasElapsed(buzzTime));
+        Trigger stopper = new Trigger(() -> pulseTimer.hasElapsed(pulseTime));
+        stopper.onTrue(new RunCommand(() -> {
+            stop();
+            stopPulse();
+        }));
+    }
+
+    public void startPulse() { // TODO: Javadoc
+        isPulsing = true;
+        pulseTimer.start();
+        Trigger stopper = new Trigger(() -> pulseTimer.hasElapsed(pulseTime));
         stopper.onTrue(new RunCommand(() -> stop()));
+        Trigger waiter = new Trigger(() -> pulseTimer.hasElapsed(pulseTime + pulseGapTime));
+        waiter.onTrue(new RunCommand(() -> {
+            if(isPulsing) start();
+            pulseTimer.reset();
+        }));
+    }
+
+    public void stopPulse() { // TODO: Javadoc
+        isPulsing = false;
+        pulseTimer.stop();
+        pulseTimer.reset();
+    }
+
+    /**
+     * Controller rumbles for a short period of time a specified number of times.
+     * <p>To change the length of a pulse, use setPulseTime. To change the time between pusles, use setPulseGapTime.
+     * @param times The desired number of pulses.
+     */
+    public void pulse(int times) {
+        isPulsing = true;
+        pulseTimer.start();
+        start();
+        for(AtomicInteger i = new AtomicInteger(0); i.get() < times; i.incrementAndGet()) {
+            Trigger stopper = new Trigger(() -> pulseTimer.hasElapsed(((pulseTime + pulseGapTime) * i.get()) + pulseTime));
+            stopper.onTrue(new RunCommand(() -> stop()));
+            if(i.get() < times - 1) {
+                Trigger waiter = new Trigger(() -> pulseTimer.hasElapsed((pulseTime + pulseGapTime) * (i.get() + 1)));
+                waiter.onTrue(new RunCommand(() -> start()));
+            }
+            else {
+                stopper.onTrue(new RunCommand(() -> stopPulse()));
+            }
+        }
     }
 
     /**
@@ -126,15 +191,52 @@ public class Rumble {
      */
     public void rumbleWhile(BooleanSupplier condition) {
         Trigger scheduler = new Trigger(condition);
-        scheduler.whileTrue(new RunCommand(() -> start())).whileFalse(new RunCommand(() -> stop()));
+        scheduler.onTrue(new RunCommand(() -> start())).onFalse(new RunCommand(() -> stop()));
     }
 
     /**
-     * Schedules the Rumble to buzz based on the boolean condition.
-     * @param condition The condition that should be true to buzz.
+     * Schedules the Rumble to start pulsing based on the boolean condition.
+     * @param condition The condition that should be true to start pulsing.
      */
-    public void buzzWhen(BooleanSupplier condition) {
+    public void startPulseWhen(BooleanSupplier condition) {
         Trigger scheduler = new Trigger(condition);
-        scheduler.onTrue(new RunCommand(() -> buzz()));
+        scheduler.onTrue(new RunCommand(() -> startPulse()));
+    }
+
+    /**
+     * Schedules the Rumble to stop pulsing based on the boolean condition.
+     * @param condition The condition that should be true to stop pulsing.
+     */
+    public void stopPulseWhen(BooleanSupplier condition) {
+        Trigger scheduler = new Trigger(condition);
+        scheduler.onTrue(new RunCommand(() -> stopPulse()));
+    }
+
+    /**
+     * Schedules the Rumble to pulse once based on the boolean condition.
+     * @param condition The condition that should be true to pulse once.
+     */
+    public void pulseWhen(BooleanSupplier condition) {
+        Trigger scheduler = new Trigger(condition);
+        scheduler.onTrue(new RunCommand(() -> pulse()));
+    }
+
+    /**
+     * Schedules the Rumble to pulse a specified number of times based on the boolean condition.
+     * @param condition The condition that should be true to pulse a specified number of times.
+     * @param times The desired number of pulses.
+     */
+    public void pulseWhen(BooleanSupplier condition, int times) {
+        Trigger scheduler = new Trigger(condition);
+        scheduler.onTrue(new RunCommand(() -> pulse(times)));
+    }
+
+    /**
+     * Schedules the Rumble to pulse when the condition is true and stop when it is false.
+     * @param condition The condition that should be true to start pulsing.
+     */
+    public void pulseWhile(BooleanSupplier condition) {
+        Trigger scheduler = new Trigger(condition);
+        scheduler.onTrue(new RunCommand(() -> start())).onFalse(new RunCommand(() -> stop()));
     }
 }
