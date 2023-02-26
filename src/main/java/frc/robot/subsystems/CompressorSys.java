a0dea1ca41be809d71c0b5b03e82334bc7704dd8
FRC6081 <firstrtm@gmail.com>
2023-02-26 00:37:17 -0600

    compressor: auto reenable after brownout

--- src/main/java/frc/robot/subsystems/CompressorSys.java
+++ src/main/java/frc/robot/subsystems/CompressorSys.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANDevices;
@@ -12,6 +13,8 @@ import frc.robot.Constants.CompressorConstants;
 public class CompressorSys extends SubsystemBase {
 
     private final Compressor compressor;
+
+    private final Timer timer;
 
     /**
      * Constructs a new CompressorSys.
@@ -23,17 +26,32 @@ public class CompressorSys extends SubsystemBase {
 
         compressor.enableAnalog(CompressorConstants.minPressurePSI, CompressorConstants.maxPressurePSI);
 
+        timer = new Timer();
     }
 
     // This method will be called once per scheduler run
     @Override
     public void periodic() {
-        if(RobotController.isBrownedOut())
+        if(RobotController.isBrownedOut()) {
             compressor.disable();
+            timer.stop();
+            timer.reset();
+        }
+
+        if(!compressor.isEnabled() && timer.get() == 0.0 && RobotController.getBatteryVoltage() > CompressorConstants.compressorReenableVoltage) {
+            timer.start();
+        }
+
+        if(timer.hasElapsed(CompressorConstants.compressorReenableSeconds)) {
+            compressor.enableAnalog(CompressorConstants.minPressurePSI, CompressorConstants.maxPressurePSI);
+            timer.stop();
+            timer.reset();
+        }
 
         SmartDashboard.putNumber("pressure PSI", compressor.getPressure());
+        SmartDashboard.putBoolean("compressor enabled", compressor.isEnabled());
 
-        if(compressor.getPressure() < 0.0)
+        if(compressor.getPressure() <= 0.0)
             DriverStation.reportError("PRESSURE RELEASE VALVE IS OPEN", false);
     }
 
