64dc5b11f2f662c86d655cf5f80a1d35c470608d
Logan-Kippnick <lgkippnick@outlook.com>
2023-02-26 22:10:36 -0500

    buttons bind on teleOp init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -11,43 +10,28 @@ public class Robot extends TimedRobot {
 
     private Command autonomousCommand;
 
-    private boolean controlsAreBinded = false;
-
     @Override
     public void robotInit() {
-
         robotContainer = new RobotContainer();
-
     }
 
     @Override
     public void robotPeriodic() {
-
-        CommandScheduler.getInstance().run();
-
-        if(DriverStation.isEnabled() && !controlsAreBinded) {
-            robotContainer.configBindings();
-            controlsAreBinded = true;
-        }
-
-        robotContainer.periodic();
-        
+        CommandScheduler.getInstance().run();  
     }
 
     @Override
     public void autonomousInit() {
-        
         autonomousCommand = robotContainer.getAutonomousCommand();
 
         if(autonomousCommand != null) autonomousCommand.schedule();
-
     }
 
     @Override
     public void teleopInit() {
-
         if(autonomousCommand != null) autonomousCommand.cancel();
 
+        robotContainer.configBindings();
     }
 
 }
