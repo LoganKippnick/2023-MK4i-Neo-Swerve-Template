960c494532fbc51782b59d0add796f42b769fdd8
Logan-Kippnick <lgkippnick@outlook.com>
2023-02-26 22:13:53 -0500

    organization

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -10,7 +10,6 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
