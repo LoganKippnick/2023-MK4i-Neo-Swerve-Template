11c1ae05e5fb14970b0386fd433658b5a3a80598
FRC6081 <firstrtm@gmail.com>
2023-02-27 22:28:03 -0600

    auto: intake control

--- src/main/java/frc/robot/commands/auto/programs/RightConeGrabCube.java
+++ src/main/java/frc/robot/commands/auto/programs/RightConeGrabCube.java
@@ -8,6 +8,10 @@ import frc.robot.commands.auto.FollowTrajectoryCmd;
 import frc.robot.commands.automation.AutoRow3PoleCmd;
 import frc.robot.commands.claw.CloseCmd;
 import frc.robot.commands.drivetrain.SetHeadingCmd;
+import frc.robot.commands.intake.InCmd;
+import frc.robot.commands.intake.OutCmd;
+import frc.robot.commands.intake.SetRelativeSpeedCmd;
+import frc.robot.commands.intake.StopRollersCmd;
 import frc.robot.subsystems.ClawSys;
 import frc.robot.subsystems.IntakeSys;
 import frc.robot.subsystems.LiftSys;
@@ -24,11 +28,11 @@ public class RightConeGrabCube extends SequentialCommandGroup {
             new FollowTrajectoryCmd("RightStartToCube", swerveSys)
                 .alongWith(
                     new WaitUntilCmd(() -> swerveSys.getPose().getX() > 4.5)
-                    .andThen() // TODO: Start and bring out intake
+                    .andThen(new OutCmd(intakeSys).alongWith(new SetRelativeSpeedCmd(intakeSys, swerveSys)))
                 ),
             new WaitCmd(1.0),
-            new CloseCmd(clawSys)
-            // TODO: Stop and bring in intake
+            new CloseCmd(clawSys),
+            new InCmd(intakeSys).alongWith(new StopRollersCmd(intakeSys))
         );
     }
 }
