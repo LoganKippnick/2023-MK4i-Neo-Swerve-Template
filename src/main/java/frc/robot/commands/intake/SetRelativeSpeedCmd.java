2d9ddb8a7b15def6400d1d8e63f8fae036573341
FRC6081 <firstrtm@gmail.com>
2023-02-27 22:29:52 -0600

    intake: driver relative speed control

---
+++ src/main/java/frc/robot/commands/intake/SetRelativeSpeedCmd.java
@@ -0,0 +1,58 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.subsystems.IntakeSys;
+import frc.robot.subsystems.SwerveSys;
+
+public class SetRelativeSpeedCmd extends CommandBase {
+
+    private final IntakeSys intakeSys;
+    private final SwerveSys swerveSys;
+
+    /**
+     * Constructs a new ExampleCmd.
+     * 
+     * <p>ExampleCmd contains the basic framework of a robot command for use in command-based programming.
+     * 
+     * <p>The command finishes once the isFinished method returns true.
+     * 
+     * @param exampleSys The required ExampleSys.
+     */
+    public SetRelativeSpeedCmd(IntakeSys intakeSys, SwerveSys swerveSys) {
+
+        this.intakeSys = intakeSys;
+        this.swerveSys = swerveSys;
+
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        intakeSys.setRelativeSpeed(IntakeConstants.rollerRelativeMetersPerSecond - swerveSys.getForwardVelocityMetersPerSecond());
+    }
+    
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+    
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    // Whether the command should run when robot is disabled.
+    @Override
+    public boolean runsWhenDisabled() {
+        return false;
+    }
+}
---
+++ src/main/java/frc/robot/commands/intake/StopRollersCmd.java
@@ -0,0 +1,53 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSys;
+
+public class StopRollersCmd extends CommandBase {
+
+    private final IntakeSys intakeSys;
+
+    /**
+     * Constructs a new ExampleCmd.
+     * 
+     * <p>ExampleCmd contains the basic framework of a robot command for use in command-based programming.
+     * 
+     * <p>The command finishes once the isFinished method returns true.
+     * 
+     * @param exampleSys The required ExampleSys.
+     */
+    public StopRollersCmd(IntakeSys intakeSys) {
+
+        this.intakeSys = intakeSys;
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        intakeSys.setRollerPower(0.0);
+    }
+    
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+    
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    // Whether the command should run when robot is disabled.
+    @Override
+    public boolean runsWhenDisabled() {
+        return false;
+    }
+}
