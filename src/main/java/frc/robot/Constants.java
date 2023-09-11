3d31bb153d2f8d9494a2ba8ef416a1c6a52a0645
LoganKippnick <lgkippnick@outlook.com>
2023-09-10 23:53:25 -0400

    stripped Charged Up code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,6 +37,8 @@ public class Constants {
         public static final int driverGamepadPort = 0;
 
         public static final double joystickDeadband = 0.15;
+
+        public static final double triggerPressedThreshhold = 0.25;
     }
     
     public static final class DriveConstants {
@@ -114,11 +116,7 @@ public class Constants {
     public static final class AutoConstants {
 
         public static final double maxVelMetersPerSec = 3.25;
-<<<<<<< HEAD
-        public static final double maxAccelMetersPerSecondSq = 1.625; //1.5
-=======
         public static final double maxAccelMetersPerSecondSq = 1.625;
->>>>>>> 6ddad2a720cbb3d96bb1f609ba37c239b20e0fa7
 
         public static final double drivekP = 12.8;
         public static final double drivekD = 0.085;
@@ -153,166 +151,5 @@ public class Constants {
             
             return rotController;
         }
-<<<<<<< HEAD
-
-        public static final double driveOntoChargeStationVelMetersPerSecond = 0.75;
-        public static final double driveOverChargeStationVelMetersPerSecond = 0.35;
-
-        public static final double driveOntoChargeStationSecs = 2.0;
-
-        public static final double onChargeStationDeg = 10.0;
-        public static final double chargeStationBalancedToleranceDeg = 11.5;
-
-        public static final double dockVelMetersPerSecond = 0.06;
-=======
->>>>>>> 6ddad2a720cbb3d96bb1f609ba37c239b20e0fa7
     }
-
-    public static final class VisionConstants {
-
-        public static final double maxAlignSpeedMetersPerSecond = 1.0;
-
-        public static final double conekP = 0.01;
-        public static final double conekD = 0.0007;
-
-        public static final PIDController coneController = new PIDController(conekP, 0.0, conekD);
-
-        public static final double cubekP = 0.004;
-        public static final double cubekD = 0.00015;
-
-        public static final PIDController cubeController = new PIDController(cubekP, 0.0, cubekD);
-
-        public static final double alignedToleranceDegrees = 2.5;
-
-        public static final double rotkP = 0.02;
-        public static final double rotkD = 0.001;
-
-        public static final double rotToleranceDeg = 2.0;
-
-        public static final PIDController rotController = constructRotController();
-        private static PIDController constructRotController() {
-            PIDController rotController = new PIDController(
-                rotkP,
-                0,
-                rotkD
-            );
-
-            // Sets wraparound from 0 to 360.
-            rotController.enableContinuousInput(0, 360);
-
-            return rotController;
-        }
-    }
-<<<<<<< HEAD
-
-    public static final class CompressorConstants {
-
-        public static final double maxPressurePSI = 120.0; // 120 PSI is the legal maximum air tank pressure.
-        public static final double minPressurePSI = 115.0;
-    }
-    
-    public static final class LiftConstants {
-
-        public static final double gearReduction = 1.0 / 5.0;
-
-        public static final double maxHeightInches = 77.0;
-        public static final double maxUnarticulatedHeightInches = 55.5;
-
-        public static final double inchesPerEncRev = 11 * gearReduction;
-
-        public static final double kP = 0.09;
-        public static final double kD = 2.0;
-
-        public static final int maxCurrentAmps = 25;
-
-        public static final double feetPerSecondPerRPM = (inchesPerEncRev / 12) / 60;
-
-        public static final double manualPower = 0.25;
-        public static final double downPower = 0.45;
-        public static final double hybridPower = 0.55;
-        public static final double placeConePower = 0.45;
-        public static final double placeCubePower = 0.6;
-        public static final double shelfPower = 0.65;
-        public static final double hoverPower = 0.3;
-
-        public static final double downInches = 0.0;
-        public static final double hoverInches = 12.0;
-        public static final double row1Inches = 35.0;
-        public static final double shelfPickupInches = 40.0;
-        public static final double shelfRaiseInches = 43.0;
-        public static final double row2ShelfInches = 49.0;
-        public static final double row2PoleInches = 55.0;
-        public static final double row3ShelfInches = 71.0;
-        public static final double row3PoleInches = maxHeightInches;
-
-        public static final double manualControlPaddingInches = 2.0;
-
-        public static final double targetToleranceInches = 0.5;
-
-        public static final double downActuationHeightInches = 18.0;
-        public static final double upActuationHeightInches = 24.0;
-
-        public static final double hybridYeetPower = 0.8;
-        public static final double hybridYeetReleaseInches = 4.0;
-        public static final double hybridYeetHeightInches = 24.0;
-
-        public static final double YEETPower = 1.0;
-        public static final double YEETReleaseInches = 26.0;
-        public static final double YEETHeightInches = 76.5;
-    }
-
-    public class IntakeConstants {
-
-        public static final double rollerGearReduction = 16.0 / 22.0;
-
-        public static final double pulleyDiameterInches = 1.0;
-        public static final double beltDiameterInches = pulleyDiameterInches + ((1.0 / 16.0) * 2.0);
-        public static final double beltCircumferenceInches = beltDiameterInches * Math.PI;
-
-        public static final int actuationEncCountsPerRev = 8192;
-        public static final double encRevToInches = beltCircumferenceInches;
-
-        public static final double absEncOffset = 0.0;
-
-        public static final int actuationCurrentLimitAmps = 30;
-        public static final int rollerCurrentLimitAmps = 60;
-
-        public static final double kP = 3.0;
-        public static final double kD = 0.0;
-
-        public static final double manualPower = 0.5;
-
-        public static final double manualControlPadding = 0.5;
-
-        public static final double freeRPM = 5600 * rollerGearReduction;
-
-        public static final double inInches = 2.0;
-        public static final double coneInches = 8.0;
-        public static final double outInches = 15.0;
-
-        public static final double intakeRollerStartInches = 0.0;//8.0;
-
-        public static final double minInches = 0.0;
-        public static final double maxInches = 17.0;
-
-        public static final double targetToleranceInches = 0.5;
-
-        public static final double clawOpenLimitInches = 11.0;
-
-        public static final double rollerWheelDiameterInches = 4.0;
-        public static final double rollerWheelCircumferenceInches = rollerWheelDiameterInches * Math.PI;
-
-        public static final double driveMetersPerSecondToRollerRPM = (39.37 * 60.0) / rollerWheelCircumferenceInches;
-
-        public static final double rollerManualControlFactor = 0.8;
-
-        public static final double rollerRelativeMetersPerSecond = 4.875;
-        public static final double rollerRelativeSpeedFactor = 0.25;
-
-        public static final double rollerAbsoluteMetersPerSecond = 4.875;
-
-        public static final double rollerConeRPM = 3000;
-    }
-=======
->>>>>>> 6ddad2a720cbb3d96bb1f609ba37c239b20e0fa7
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,36 +1,15 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.ControllerConstants;
 import frc.robot.commands.drivetrain.LockCmd;
 import frc.robot.commands.drivetrain.ResetHeadingCmd;
 import frc.robot.commands.drivetrain.SwerveDriveCmd;
-import frc.robot.commands.intake.InCmd;
-import frc.robot.commands.intake.IntakeConeCmd;
-import frc.robot.commands.intake.OutCmd;
-import frc.robot.commands.intake.IntakeCubeCmd;
-import frc.robot.commands.intake.StopRollersCmd;
-import frc.robot.commands.intake.IntakeManualControlCmd;
-import frc.robot.commands.lift.DownCmd;
-import frc.robot.commands.lift.HoverCmd;
-import frc.robot.commands.lift.LiftManualControlCmd;
-import frc.robot.commands.lift.Row1Cmd;
-import frc.robot.commands.lift.Row2Cmd;
-import frc.robot.commands.lift.Row3Cmd;
-import frc.robot.commands.lift.ShelfPickupCmd;
-import frc.robot.commands.lights.TogglePartyModeCmd;
-import frc.robot.commands.lights.ToggleWeeWooModeCmd;
-import frc.robot.commands.vision.RestartLimelightCmd;
-import frc.robot.subsystems.ClawSys;
-import frc.robot.subsystems.CompressorSys;
-import frc.robot.subsystems.IntakeSys;
-import frc.robot.subsystems.LiftSys;
-import frc.robot.subsystems.LightsSys;
 import frc.robot.subsystems.SwerveSys;
 
 public class RobotContainer {
@@ -41,61 +20,11 @@ public class RobotContainer {
     // Initialize joysticks.
     private final XboxController driverController = new XboxController(ControllerConstants.driverGamepadPort);
 
-    // Initialize controller buttons.
-    private final JoystickButton driverRightJoystickTriggerBtn = new JoystickButton(driverRightJoystick, 1);
-    private final JoystickButton driverRightJoystickThumbBtn = new JoystickButton(driverRightJoystick, 2);
-
-    private final JoystickButton driverABtn = new JoystickButton(driverController, 1);
-    // private final JoystickButton driverBBtn = new JoystickButton(driverController, 2);
-    private final JoystickButton driverXBtn = new JoystickButton(driverController, 3);
-    private final JoystickButton driverYBtn = new JoystickButton(driverController, 4);
-    // private final JoystickButton driverLeftBumper = new JoystickButton(driverController, 5);
-    private final JoystickButton driverRightBumper = new JoystickButton(driverController, 6);
     private final JoystickButton driverMenuBtn = new JoystickButton(driverController, 8);
-    private final Trigger driverRightTriggerBtn =
-        new Trigger(() -> driverController.getRightTriggerAxis() > ControllerConstants.triggerPressedDeadband);
+
     private final Trigger driverLeftTriggerBtn =
-        new Trigger(() -> driverController.getLeftTriggerAxis() > ControllerConstants.triggerPressedDeadband);
+        new Trigger(() -> driverController.getLeftTriggerAxis() > ControllerConstants.triggerPressedThreshhold);
 
-    private final JoystickButton operatorABtn = new JoystickButton(operatorController, 1);
-    private final JoystickButton operatorBBtn = new JoystickButton(operatorController, 2);
-    private final JoystickButton operatorXBtn = new JoystickButton(operatorController, 3);
-    private final JoystickButton operatorYBtn = new JoystickButton(operatorController, 4);
-    private final JoystickButton operatorLeftBumper = new JoystickButton(operatorController, 5);
-    private final JoystickButton operatorRightBumper = new JoystickButton(operatorController, 6);
-    private final JoystickButton operatorWindowBtn = new JoystickButton(operatorController, 7);
-    private final JoystickButton operatorMenuBtn = new JoystickButton(operatorController, 8);
-    private final POVButton operatorUpBtn = new POVButton(operatorController, 0);
-    private final POVButton operatorRightBtn = new POVButton(operatorController, 90);
-    private final POVButton operatorDownBtn = new POVButton(operatorController, 180);
-    private final POVButton operatorLeftBtn = new POVButton(operatorController, 270);
-
-    private final JoystickButton hybridABtn = new JoystickButton(hybridController, 1);
-    private final JoystickButton hybridBBtn = new JoystickButton(hybridController, 2);
-    private final JoystickButton hybridXBtn = new JoystickButton(hybridController, 3);
-    private final JoystickButton hybridYBtn = new JoystickButton(hybridController, 4);
-    private final JoystickButton hybridLeftBumper = new JoystickButton(hybridController, 5);
-    private final JoystickButton hybridRightBumper = new JoystickButton(hybridController, 6);
-    private final JoystickButton hybridWindowBtn = new JoystickButton(hybridController, 7);
-    private final JoystickButton hybridMenuBtn = new JoystickButton(hybridController, 8);
-    private final JoystickButton hybridRightJoystickPressBtn = new JoystickButton(hybridController, 10);
-    
-    private final Trigger hybridRightTriggerBtn =
-        new Trigger(() -> hybridController.getRightTriggerAxis() > ControllerConstants.triggerPressedDeadband);
-    private final Trigger hybridLeftTriggerBtn =
-        new Trigger(() -> hybridController.getLeftTriggerAxis() > ControllerConstants.triggerPressedDeadband);
-
-    private final POVButton hybridUpBtn = new POVButton(hybridController, 0);
-    private final POVButton hybridRightBtn = new POVButton(hybridController, 90);
-    private final POVButton hybridDownBtn = new POVButton(hybridController, 180);
-    private final POVButton hybridLeftBtn = new POVButton(hybridController, 270);
-
-    // Instantiate controller rumble.
-    private Rumble matchTimeRumble;
-    private Rumble brownOutRumble;
-    private Rumble countdown10Rumble;
-    private Rumble countdown5Rumble;
-    private Rumble targetAlignedRumble;
 
     // Initialize auto selector.
     SendableChooser<Command> autoSelector = new SendableChooser<Command>();
@@ -104,199 +33,20 @@ public class RobotContainer {
         SmartDashboard.putData("auto selector", autoSelector);
 
         configDriverBindings();
-        configOperatorBindings();
     }
 
-    public void configBindings() {
-        lightsSys.cancelAnimations();
-
-        brownOutRumble = new Rumble(RumbleType.kLeftRumble, 1.0, driverController);
-        matchTimeRumble = new Rumble(RumbleType.kRightRumble, 1.0, driverController);
-        countdown10Rumble = new Rumble(RumbleType.kRightRumble, 1.0, driverController);
-        countdown5Rumble = new Rumble(RumbleType.kRightRumble, 1.0, driverController);
-        targetAlignedRumble = new Rumble(RumbleType.kLeftRumble, 1.0, driverController);
-
-        if(DriverStation.isJoystickConnected(ControllerConstants.hybridControllerPort)) {
-            configHybridBindings();
-            // SmartDashboard.putString("control type", "hybrid");
-        }
-        else {
-            if(DriverStation.getJoystickIsXbox(ControllerConstants.driverGamepadPort)) {
-                configDriverBindings(ControllerType.kGamepad);
-                // SmartDashboard.putString("control type", "gamepad");
-            }
-            else if(DriverStation.isJoystickConnected(ControllerConstants.driverRightJoystickPort)) {
-                configDriverBindings(ControllerType.kJoystick);
-                // SmartDashboard.putString("control type", "joysticks");
-            }
-            else {
-                // SmartDashboard.putString("control type", "only operator");
-                brownOutRumble = new Rumble(RumbleType.kRightRumble, 1.0);
-            }
-
-            configOperatorBindings();
-        }
-
-        brownOutRumble.rumbleWhen(() -> RobotController.isBrownedOut(), 2.0);
-        
-        matchTimeRumble.pulseWhen(() -> DriverStation.getMatchTime() <= 60.0 && DriverStation.isTeleop(), 3);
-        matchTimeRumble.pulseWhen(() -> DriverStation.getMatchTime() <= 30.0 && DriverStation.isTeleop(), 2);
-        matchTimeRumble.pulseWhen(() -> DriverStation.getMatchTime() <= 15.0 && DriverStation.isTeleop(), 1);
-
-        countdown10Rumble.setPulseTime(1.0);
-        countdown10Rumble.pulseWhen(() -> DriverStation.getMatchTime() <= 10.0 && DriverStation.isTeleop(), 5);
-
-        countdown5Rumble.setPulseTime(1.0);
-        countdown5Rumble.setPulseLength(0.25);
-        countdown5Rumble.pulseWhen(() -> DriverStation.getMatchTime() <= 5.0 && DriverStation.isTeleop(), 5);
-
-        targetAlignedRumble.pulseWhen(() -> visionSys.targetIsXAligned() && visionSys.hasTarget() && swerveSys.isTracking());
-    }
-
-    public void configDriverBindings(ControllerType driverControllerType) {
-        if(driverControllerType.equals(ControllerType.kGamepad)) {
-            swerveSys.setDefaultCommand(
-                new SwerveDriveCmd(
-                    () -> deadband(driverController.getLeftY(), driverControllerType),
-                    () -> deadband(driverController.getLeftX(), driverControllerType),
-                    () -> deadband(driverController.getRightX(), driverControllerType),
-                    true,
-                    swerveSys
-                )
-            );
-
-            driverABtn.onTrue(new ToggleWeeWooModeCmd(lightsSys));
-            driverXBtn.whileTrue(new AutoAlignCmd(
-                () -> deadband(driverController.getLeftY(), driverControllerType),
-                () -> deadband(driverController.getLeftX(), driverControllerType),
-                visionSys, swerveSys, liftSys)
-            );
-            driverYBtn.onTrue(new TogglePartyModeCmd(lightsSys));
+    public void configDriverBindings() {
+        new SwerveDriveCmd(
+            () -> deadband(driverController.getLeftY()),
+            () -> deadband(driverController.getLeftX()),
+            () -> deadband(driverController.getRightX()),
+            true,
+            swerveSys
+        );
             
-            driverMenuBtn.onTrue(new ResetHeadingCmd(swerveSys));
-
-            driverRightTriggerBtn
-                .onTrue(new OutCmd(intakeSys, lightsSys))
-                .onTrue(new IntakeCubeCmd(intakeSys, lightsSys))
-                .onFalse(new InCmd(intakeSys))
-                .onFalse(new StopRollersCmd(intakeSys, lightsSys));
-
-            driverRightBumper
-                .onTrue(new OutCmd(intakeSys, lightsSys))
-                .whileTrue(new IntakeConeCmd(intakeSys, lightsSys))
-                .onFalse(new InCmd(intakeSys))
-                .onFalse(new StopRollersCmd(intakeSys, lightsSys));
-
-            driverLeftTriggerBtn.whileTrue(new LockCmd(swerveSys));
-
-            brownOutRumble.addControllers(driverController);
-            matchTimeRumble.addControllers(driverController);
-            countdown10Rumble.addControllers(driverController);
-            countdown5Rumble.addControllers(driverController);
-            targetAlignedRumble.addControllers(driverController);
-        }
-        else {
-            swerveSys.setDefaultCommand(
-                new SwerveDriveCmd(
-                    () -> deadband(driverLeftJoystick.getY(), driverControllerType),
-                    () -> deadband(driverLeftJoystick.getX(), driverControllerType),
-                    () -> deadband(driverRightJoystick.getX(), driverControllerType),
-                    true,
-                    swerveSys
-                )
-            );
-
-            driverRightJoystickTriggerBtn
-                .onTrue(new OutCmd(intakeSys, lightsSys))
-                .whileTrue(new IntakeCubeCmd(intakeSys, lightsSys))
-                .onFalse(new InCmd(intakeSys))
-                .onFalse(new StopRollersCmd(intakeSys, lightsSys));
-
-            driverRightJoystickThumbBtn.onTrue(new ResetHeadingCmd(swerveSys));
-        }
-    }
-
-    public void configOperatorBindings() {
-        liftSys.setDefaultCommand(
-            new LiftManualControlCmd(
-                () -> deadband(operatorController.getRightY(), ControllerType.kGamepad),
-                liftSys
-            )
-        );
-
-        intakeSys.setDefaultCommand(
-            new IntakeManualControlCmd(
-                () -> deadband(operatorController.getLeftY(), ControllerType.kGamepad),
-                () -> deadband(operatorController.getRightTriggerAxis(), ControllerType.kGamepad),
-                () -> deadband(operatorController.getLeftTriggerAxis(), ControllerType.kGamepad),
-                intakeSys
-            )
-        );
-
-        operatorABtn.onTrue(new Row1Cmd(true, liftSys));
-        operatorBBtn.onTrue(new Row2Cmd(lightsSys, true, liftSys));
-        operatorXBtn.onTrue(new DownCmd(true, liftSys));
-        operatorYBtn.onTrue(new Row3Cmd(lightsSys, true, liftSys));
-
-        operatorWindowBtn.onTrue(new SetElementStatusCmd(GameElement.kCube, liftSys, intakeSys, visionSys, lightsSys));
-        operatorMenuBtn.onTrue(new SetElementStatusCmd(GameElement.kCone, liftSys, intakeSys, visionSys, lightsSys));
-
-        operatorWindowBtn.and(operatorMenuBtn).onTrue(new SetElementStatusCmd(GameElement.kNone, liftSys, intakeSys, visionSys, lightsSys));
-        
-        operatorLeftBumper.onTrue(new OpenCmd(clawSys));
-        operatorRightBumper.onTrue(new CloseCmd(clawSys));
-
-        operatorUpBtn.onTrue(new ShelfPickupCmd(true, liftSys));
-        operatorRightBtn.onTrue(new YEETCmd(liftSys, clawSys));
-        operatorDownBtn.onTrue(new HoverCmd(true, liftSys));
-        operatorLeftBtn.onTrue(new HybridYeetCmd(liftSys, clawSys));
-
-        matchTimeRumble.addControllers(operatorController);
-        countdown10Rumble.addControllers(operatorController);
-        countdown5Rumble.addControllers(operatorController);
-        targetAlignedRumble.addControllers(operatorController);
-    }
-
-    public void configHybridBindings() {
-        swerveSys.setDefaultCommand(
-            new SwerveDriveCmd(
-                () -> deadband(driverController.getLeftY()),
-                () -> deadband(driverController.getLeftX()),
-                () -> deadband(driverController.getRightX()),
-                true,
-                swerveSys
-            )
-        );
-
-        driverRightBumper.whileTrue(new LockCmd(swerveSys));
-
         driverMenuBtn.onTrue(new ResetHeadingCmd(swerveSys));
-    }
 
-        hybridWindowBtn.and(hybridMenuBtn).onTrue(new SetElementStatusCmd(GameElement.kNone, liftSys, intakeSys, visionSys, lightsSys));
-        
-        hybridLeftBumper.onTrue(new OpenCmd(clawSys));
-        hybridRightBumper.onTrue(new CloseCmd(clawSys));
-
-        hybridRightJoystickPressBtn.onTrue(new ResetHeadingCmd(swerveSys));
-
-        hybridRightTriggerBtn
-                .onTrue(new OutCmd(intakeSys, lightsSys))
-                .whileTrue(new RepeatCommand(new IntakeCubeCmd(intakeSys, lightsSys)))
-                .onFalse(new InCmd(intakeSys))
-                .onFalse(new StopRollersCmd(intakeSys, lightsSys));
-        hybridLeftTriggerBtn.whileTrue(new LockCmd(swerveSys));
-
-        hybridUpBtn.onTrue(new ShelfPickupCmd(true, liftSys));
-        hybridRightBtn.onTrue(new YEETCmd(liftSys, clawSys));
-        hybridDownBtn.onTrue(new HoverCmd(true, liftSys));
-        hybridLeftBtn.onTrue(new HybridYeetCmd(liftSys, clawSys));
-
-        brownOutRumble.addControllers(hybridController);
-        matchTimeRumble.addControllers(hybridController);
-        countdown10Rumble.addControllers(hybridController);
-        countdown5Rumble.addControllers(hybridController);
-        targetAlignedRumble.addControllers(hybridController);
+        driverLeftTriggerBtn.whileTrue(new LockCmd(swerveSys));
     }
 
     public Command getAutonomousCommand() {
@@ -321,18 +71,7 @@ public class RobotContainer {
     }
 
     public void updateInterface() {
-        // BATTERY
-        SmartDashboard.putNumber("battery voltage", RobotController.getBatteryVoltage());
-
-        // SWERVE
-        double headingDisplay = swerveSys.getHeading().getDegrees() % 360;
-        if(headingDisplay < 0) {
-            headingDisplay += 360;
-        }
-
-        SmartDashboard.putNumber("heading", headingDisplay);
-
+        SmartDashboard.putNumber("heading degrees", swerveSys.getHeading().getDegrees());
         SmartDashboard.putNumber("speed m/s", swerveSys.getAverageDriveVelocityMetersPerSecond());
-        SmartDashboard.putNumber("speed mph", swerveSys.getAverageDriveVelocityMetersPerSecond() * 2.23694);
     }
 }
